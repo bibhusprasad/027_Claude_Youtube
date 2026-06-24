c3c3cccb05e9140001981f341fae808b9a7a7ef9
bibhusprasad <bibhusprasad@gmail.com>
2026-06-24 23:23:27 +0530

    added rules

---
+++ numbers.py
@@ -0,0 +1,11 @@
+def add(a, b):
+    """
+    Adds two numbers together.
+    
+    Args:
+        a (int/float): First number
+        b (int/float): Second number
+    Returns:
+        int/float: Sum of a and b
+    """
+    return a + b
